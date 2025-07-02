1349732f5d301c47654e586d870dc4906ce93b7c
a.turtschin <turtschin.a@web.de>
2025-07-02 10:52:30 +0200

    init commit

---
+++ server1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        
+        var ip = IPAddress.Any;
+        var port = 12345;
+        var server = new TcpListener(ip, port);
+
+        server.Start();
+        Console.WriteLine($"server lauft auf {ip}:{port}...");
+
+        while (true)
+        {
+            using (var client = server.AcceptTcpClient())
+            using (var stream = client.GetStream())
+            {
+                var clientIp = ((IPEndPoint)client.Client.RemoteEndPoint).Address;
+                Console.WriteLine($"Client ist verbunden: {clientIp}");
+
+                var buffer = new byte[1024];
+                var bytesRead = stream.Read(buffer, 0, buffer.Length);
+                var message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine($"Bekam message: {message}");
+
+                var response = $"server bekam message: {message}";
+                var responseData = Encoding.UTF8.GetBytes(response);
+                stream.Write(responseData, 0, responseData.Length);
+            }
+        }
+    }
+}
---
+++ server2_client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        using (var client = new TcpClient("localhost", 12345))
+        using (var stream = client.GetStream())
+        {
+
+            var message = "Hallo, server!";
+            var data = Encoding.UTF8.GetBytes(message);
+            stream.Write(data, 0, data.Length);
+
+
+            var buffer = new byte[1024];
+            var bytesRead = stream.Read(buffer, 0, buffer.Length);
+            Console.WriteLine($"Antwort von server: {Encoding.UTF8.GetString(buffer, 0, bytesRead)}");
+        }
+
+        Console.ReadLine();
+    }
+}
